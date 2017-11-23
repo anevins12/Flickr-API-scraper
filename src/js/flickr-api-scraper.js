00a037994f084e70094b006834332cb052d6509d
Andrew Nevins <andrew.nevins.misc@gmail.com>
2017-11-23 20:14:21 +0000

    #3: Added basic toggle logic

--- src/js/flickr-api-scraper.js
+++ src/js/flickr-api-scraper.js
@@ -7,4 +7,50 @@ function init() {
 	 * Initialisation
 	 */
 	'use strict';
+
+	var detailTriggers = $('[data-open-detail]'),
+		listingTriggers = $('[data-open-listing]'),
+		templates = {
+			detail: $('#template-detail'),
+			listing: $('#template-listing')
+		};
+
+	toggleTemplate(templates.detail, templates.listing, true);
+
+	if (detailTriggers.length !== 0) {
+
+		detailTriggers.on('click', function() {
+			toggleTemplate(templates.listing, templates.detail);
+		});
+	}
+
+	if (listingTriggers.length !== 0) {
+
+		listingTriggers.on('click', function() {
+			toggleTemplate(templates.detail, templates.listing);
+		});
+	}
+}
+
+function toggleTemplate(current, next, initial) {
+	/*
+	 * Shows the next template
+	 */
+
+	current.hide();
+	next.show();
+
+	// Don't move focus on initial load
+	if (!initial){
+		handleFocus(next);
+	}
+}
+
+function handleFocus(target) {
+	/*
+	 * Moves the keyboard focus
+	 */
+	target
+		.attr('tabindex', '-1')
+		.focus();
 }
