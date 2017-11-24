210b345d0b212da5c7e5a0b4717dc67bb6d7edaf
Andrew Nevins <andrew.nevins.misc@gmail.com>
2017-11-24 23:46:20 +0000

    #4: Populating feed title, media, link.

--- src/js/flickr-api-scraper.js
+++ src/js/flickr-api-scraper.js
@@ -8,6 +8,13 @@ function init() {
 	 */
 	'use strict';
 
+	bindEvents();
+	handleAPI();
+}
+
+function bindEvents() {
+	'use strict';
+
 	var detailTriggers = $('[data-open-detail]'),
 		listingTriggers = $('[data-open-listing]'),
 		templates = {
@@ -15,11 +22,13 @@ function init() {
 			listing: $('#template-listing')
 		};
 
+	// Hide detail template
 	toggleTemplate(templates.detail, templates.listing, true);
 
 	if (detailTriggers.length !== 0) {
 
 		detailTriggers.on('click', function() {
+			// Show detail template
 			toggleTemplate(templates.listing, templates.detail);
 		});
 	}
@@ -27,6 +36,7 @@ function init() {
 	if (listingTriggers.length !== 0) {
 
 		listingTriggers.on('click', function() {
+			// Show listing template
 			toggleTemplate(templates.detail, templates.listing);
 		});
 	}
@@ -36,12 +46,13 @@ function toggleTemplate(current, next, initial) {
 	/*
 	 * Shows the next template
 	 */
+	'use strict';
 
 	current.hide();
 	next.show();
 
 	// Don't move focus on initial load
-	if (!initial){
+	if (!initial) {
 		handleFocus(next);
 	}
 }
@@ -50,7 +61,76 @@ function handleFocus(target) {
 	/*
 	 * Moves the keyboard focus
 	 */
+	'use strict';
+
 	target
 		.attr('tabindex', '-1')
 		.focus();
+}
+
+function handleAPI() {
+	/*
+	 *
+	 * To avoid embedding markup inside of the script,
+	 * the current list item will be cloned for each item
+ 	 */
+	'use strict';
+
+	var args = '?tags=potato&tagmode=all&format=json&jsoncallback=?',
+		api = 'https://api.flickr.com/services/feeds/photos_public.gne' + args;
+
+	$.getJSON(api, function(data) {
+		var wrapper = $('[data-feed-wrapper]'),
+			containerIdentifier = '[data-feed-container]',
+			feedMarkup = {
+				list: $('[data-feed-list]', wrapper),
+				container: $(containerIdentifier, wrapper),
+				title: '[data-feed-title]',
+				media: '[data-feed-img]',
+				link: '[data-feed-reference]',
+				date_taken: '[data-feed-date]',
+				author:  '[data-feed-author]',
+				tag: '[data-feed-tag]'
+			},
+			responseItems = data.items,
+			responseLength = responseItems.length,
+			count = 0;
+
+		// Only looping 18 items as the markup already contains a list item
+		for (count; count < responseLength -1; count++) {
+			var clonedContainer = feedMarkup.container.clone();
+
+			feedMarkup.list.append(clonedContainer);
+		}
+
+		// For new buttons in the DOM
+		bindEvents();
+
+		$.each(responseItems, function(index, item) {
+			var responseItem = $(item).get(0),
+				// All list items
+				listItems = $(containerIdentifier),
+				// Filter the current list item
+				listItem = listItems.get(index),
+				responseDate = responseItem.published,
+				responseDay,
+				responseMonth,
+				responseYear;
+
+			// Reformat date
+			responseDate = new Date(responseDate);xss
+
+			// Populate: Title
+			$(feedMarkup.title, listItem).html(responseItem.title);
+			// Populate: Image
+			$(feedMarkup.media, listItem).attr({
+				'src': responseItem.media.m,
+				'alt': responseItem.title
+			});
+			// Populate: Link
+			$(feedMarkup.link, listItem).attr('href', responseItem.link);
+			// Populate: Published
+			$(feedMarkup.date_taken, listItem).html(responseItem.published);
+		});
+	});
 }
